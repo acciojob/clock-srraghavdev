46a463645bae495c1959e9c16210735574f3f45a
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 22:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //your JS code here. If required.
-let f= get.ElementById("timer")
-f.innerText= `${Date.now()}`
+let f= document.getElementById("timer")
+f.innerText= `${getdate()}`
+
+function getdate(){
+const d = new Date();
+d.toISOString()
+return `${d.substring(8,10)}/${d.substring(5,7)}/${d.substring(0,4)}`	
+}
