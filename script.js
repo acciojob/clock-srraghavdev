3f8180e4e44b847f219335152748e16a6e6962d7
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-22 02:08:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,28 +3,20 @@ function timeCount(){
 let time=""
 let dater=""
 let f= document.getElementById("timer")
-getdate()
-getime()
-f.innerText= `${dater}, ${time}`
-
-function getdate(){
 let d = new Date();
 d=String(d.toISOString())
 dater=`${d.substring(8,10)}/${d.substring(5,7)}/${d.substring(0,4)}`	
-}
-
-function getime(){
-	let today = new Date()
-	var hour = today.getHours();
-	let cd="AM"
-	if(hour<10)hour = "0"+hour;
-	if(hour>=12){ hour= hour-12
+let today = new Date()
+var hour = today.getHours();
+let cd="AM"
+if(hour<10)hour = "0"+hour;
+if(hour>=12){ hour= hour-12
 				cd="PM"}
-	var minute = today.getMinutes();
-	if(minute<10)minute = "0"+minute;
-	var second = today.getSeconds();
-	if(second<10)second = "0"+second;
-	time=`${hour}:${minute}:${second} ${cd}`
-	setTimeout("getime()",1000)
-}
+var minute = today.getMinutes();
+if(minute<10)minute = "0"+minute;
+var second = today.getSeconds();
+if(second<10)second = "0"+second;
+time=`${hour}:${minute}:${second} ${cd}`
+f.innerText= `${dater}, ${time}`
+setTimeout("timeCount()",1000)
 }
